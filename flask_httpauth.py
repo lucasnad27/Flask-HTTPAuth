6e8367bca63d16a111ae4fa0dbe8598b0e63c331
Luke <lucasnad27@gmail.com>
2014-09-19 14:57:31 -0700

    Appended '-x' to WWW-Authenticate header in order for a default browser window to pop-up

--- flask_httpauth.py
+++ flask_httpauth.py
@@ -36,7 +36,7 @@ class HTTPAuth(object):
                 res = make_response(res)
                 res.status_code = 401
             if 'WWW-Authenticate' not in res.headers.keys():
-                res.headers['WWW-Authenticate'] = self.authenticate_header()
+                res.headers['WWW-Authenticate-x'] = self.authenticate_header()
             return res
         self.auth_error_callback = decorated
         return decorated
